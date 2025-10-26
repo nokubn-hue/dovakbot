184188554a3638bab6fbf60c35c66f09529c5998
nokubn-hue <nokubn@gmail.com>
2025-10-26 17:38:55 +0900

    Update casinoGames_manual.js

--- casinoGames_manual.js
+++ casinoGames_manual.js
@@ -7,9 +7,7 @@ function createDeck() {
   const suits = ['♠','♥','♦','♣'];
   const ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
   const deck = [];
-  for (const s of suits) {
-    for (const r of ranks) deck.push({ rank:r, suit:s });
-  }
+  for (const s of suits) for (const r of ranks) deck.push({ rank:r, suit:s });
   for (let i=deck.length-1;i>0;i--){
     const j=Math.floor(Math.random()*(i+1));
     [deck[i],deck[j]]=[deck[j],deck[i]];
