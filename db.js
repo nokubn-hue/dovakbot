994a349360cd65aa927804e53641ecb829a52140
nokubn-hue <nokubn@gmail.com>
2025-10-26 17:38:14 +0900

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,8 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// __dirname 정의 (ESM에서 필요)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const filePath = path.join(__dirname, 'userData.json');
 
 function loadData() {
