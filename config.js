3c126433f77ce6b52033c5fd78b9a3ab6cb987a7
nokubn-hue <nokubn@gmail.com>
2025-11-02 20:51:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || '';
-export const CLIENT_ID = process.env.CLIENT_ID || '';
-export const GUILD_ID = process.env.GUILD_ID ? process.env.GUILD_ID.split(',').map(id => id.trim()) : [];
-export const ADMIN_USER_IDS = process.env.ADMIN_USER_IDS ? process.env.ADMIN_USER_IDS.split(',').map(id => id.trim()) : [];
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+export const CLIENT_ID = process.env.CLIENT_ID;
+export const GUILD_ID = process.env.GUILD_ID;
+export const ADMIN_USER_IDS = (process.env.ADMIN_USER_IDS || "").split(',').map(id => id.trim());
 export const PORT = process.env.PORT || 10000;
-export const KEEPALIVE_URL = process.env.KEEPALIVE_URL || '';
+export const KEEPALIVE_URL = process.env.KEEPALIVE_URL;
